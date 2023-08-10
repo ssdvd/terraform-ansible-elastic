8676df60591a117ec7ae7ffb99f85a334cc8f052
ssdvd <david.santana98@hotmail.com>
2023-08-09 23:53:17 -0300

    script sh de config

--- infra/main.tf
+++ infra/main.tf
@@ -22,6 +22,7 @@ resource "aws_launch_template" "maquina" {
     Name = "Terraform Ansible Python"
   }
   security_group_names = [var.sg_env]
+  user_data = filebase64("ansible.sh")
 }
 
 resource "aws_key_pair" "chaveSSH" {
