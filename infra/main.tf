48458225d3575448a4fd907e467642547af3a560
ssdvd <david.santana98@hotmail.com>
2023-08-10 23:41:03 -0300

    teste de carga

--- infra/main.tf
+++ infra/main.tf
@@ -22,12 +22,12 @@ resource "aws_launch_template" "maquina" {
     Name = "Terraform Ansible Python"
   }
   security_group_names = [var.sg_env]
-  user_data = filebase64("ansible.sh")
+  user_data            = filebase64("ansible.sh")
 }
 
 resource "aws_key_pair" "chaveSSH" {
-  key_name = var.chave
-  public_key = file("${var.chave}.pub") 
+  key_name   = var.chave
+  public_key = file("${var.chave}.pub")
 }
 
 /* output "IP_publico" {
@@ -35,12 +35,47 @@ resource "aws_key_pair" "chaveSSH" {
 } */
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = ["${var.regiao_aws}a"]
-  name = var.nomeGrupo
-  max_size = var.maximo
-  min_size = var.minimo
+  availability_zones = ["${var.regiao_aws}a", "${var.regiao_aws}b"]
+  name               = var.nomeGrupo
+  max_size           = var.maximo
+  min_size           = var.minimo
   launch_template {
-    id = aws_launch_template.maquina.id
+    id      = aws_launch_template.maquina.id
     version = "$Latest"
+  }
+  target_group_arns = [aws_lb_target_group.alvoLoadBalancer.arn]
+}
+
+resource "aws_default_subnet" "subnet-1" {
+  availability_zone = "${var.regiao_aws}a"
+}
+
+resource "aws_default_subnet" "subnet-2" {
+  availability_zone = "${var.regiao_aws}b"
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets  = [aws_default_subnet.subnet-1.id, aws_default_subnet.subnet-2.id ]
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name     = "maquinasAlvo"
+  port     = "8000"
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port              = "8000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
   }
 }
